7bcf36ad1eff0b149790ca459ba9dd6056702f0f
Bjornkrait <bjornankrait@gmail.com>
2026-06-07 21:05:09 +0500

    Почти доделаные медиа

--- js/script.js
+++ js/script.js
@@ -5,9 +5,24 @@ console.log(btn)
 const spclItem = document.querySelectorAll(".spcl-item")
 console.log(spclItem)
 const btnRem = btn[0]
+const btnNav = document.querySelector(".nav-btn")
+const navMenu = document.querySelector(".head-a")
+
+navMenu.classList.add('head-a-active')
+
+btnNav.addEventListener('click', () => {
+    if (!navMenu.classList.contains('head-a-first-active')) {
+        navMenu.classList.add('head-a-first-active')
+    }
+    else {
+        navMenu.classList.toggle('head-a-active')
+        navMenu.classList.toggle('head-a-disabled')
+    }
+    btnNav.classList.toggle('nav-menu-active')
+})
 
 btn.forEach((elm) => {
-    
+
     elm.addEventListener('click', function () {
         btn.forEach((j) => {
             j.removeAttribute('disabled', "")
@@ -24,14 +39,13 @@ btn.forEach((elm) => {
         })
 
 
-
+        elm.setAttribute("disabled", "")
         setTimeout(() => {
             if (elm.classList.contains('spcl-btn1')) {
                 spclItem[0].classList.add("spcl-item-show")
             }
             if (elm.classList.contains('spcl-btn2')) {
                 spclItem[1].classList.add("spcl-item-show")
-
             }
             if (elm.classList.contains('spcl-btn3')) {
                 spclItem[2].classList.add("spcl-item-show")
@@ -42,10 +56,8 @@ btn.forEach((elm) => {
             if (elm.classList.contains('spcl-btn5')) {
                 spclItem[4].classList.add("spcl-item-show")
             }
-            elm.setAttribute("disabled", "")
         }, 500)
 
-
-
     })
+
 });
